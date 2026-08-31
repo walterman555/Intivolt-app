7989b17bec915c7f64724b5319c0aab7cefa1c0c
walterman555 <walterman555@gmail.com>
2026-09-01 00:33:03 +0200

    Primera versión funcional INTIVOLT

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="intivolt-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json","./sw.js"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let y=x.clone();caches.open(C).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match("./index.html")))));
